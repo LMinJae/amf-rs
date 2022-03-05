f52c9400dfe28fb7ca53cef586f431f73bab86c2
Min-Jae, Lee <lee.min.jae.96@gmail.com>
2022-03-05 23:52:07 +0900

    Make object index-able

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
+use core::ops::Index;
+use core::hash::Hash;
+use core::borrow::Borrow;
+
 pub mod amf0;
 
 #[cfg(test)]
 mod tests;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Value {
     Amf0Value(amf0::Value),
 }
@@ -15,7 +19,18 @@ pub type String = std::string::String;
 pub type Property<T> = std::collections::HashMap<String, T>;
 #[derive(Debug, PartialEq, Clone)]
 pub struct Object<T> {
-    class_name: String,
-    property: Property<T>,
+    pub class_name: String,
+    pub property: Property<T>,
+}
+impl<Q: ?Sized, T> Index<&'_ Q> for Object<T>
+where
+    String: Borrow<Q>,
+    Q: Eq + Hash
+{
+    type Output = T;
+
+    fn index(&self, key: &'_ Q) -> &Self::Output {
+        self.property.index(key)
+    }
 }
 pub type Array<T> = Vec<T>;
