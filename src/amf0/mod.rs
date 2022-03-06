5627e98492e5dc663331c1f80cbe83d10e82b56e
Min-Jae, Lee <lee.min.jae.96@gmail.com>
2022-03-06 19:17:04 +0900

    Align const define

--- src/amf0/mod.rs
+++ src/amf0/mod.rs
@@ -4,23 +4,23 @@ pub mod encoder;
 // 2.1 Types Overview
 mod marker {
     #![allow(dead_code)]
-    pub const NUMBER: u8 = 0x00;
-    pub const BOOLEAN: u8 = 0x01;
-    pub const STRING: u8 = 0x02;
-    pub const OBJECT: u8 = 0x03;
-    pub const MOVIECLIP: u8 = 0x04; // reserved, not supported
-    pub const NULL: u8 = 0x05;
-    pub const UNDEFINED: u8 = 0x06;
-    pub const REFERENCE: u8 = 0x07;
-    pub const ECMA_ARRAY: u8 = 0x08;
-    pub const OBJECT_END: u8 = 0x09;
-    pub const STRICT_ARRAY: u8 = 0x0A;
-    pub const DATE: u8 = 0x0B;
-    pub const LONG_STRING: u8 = 0x0C;
-    pub const UNSUPPORTED: u8 = 0x0D;
-    pub const RECORDSET: u8 = 0x0E; // reserved, not supported
-    pub const XML_DOCUMENT: u8 = 0x0F;
-    pub const TYPED_OBJECT: u8 = 0x10;
+    pub const NUMBER        : u8 = 0x00;
+    pub const BOOLEAN       : u8 = 0x01;
+    pub const STRING        : u8 = 0x02;
+    pub const OBJECT        : u8 = 0x03;
+    pub const MOVIECLIP     : u8 = 0x04; // reserved, not supported
+    pub const NULL          : u8 = 0x05;
+    pub const UNDEFINED     : u8 = 0x06;
+    pub const REFERENCE     : u8 = 0x07;
+    pub const ECMA_ARRAY    : u8 = 0x08;
+    pub const OBJECT_END    : u8 = 0x09;
+    pub const STRICT_ARRAY  : u8 = 0x0A;
+    pub const DATE          : u8 = 0x0B;
+    pub const LONG_STRING   : u8 = 0x0C;
+    pub const UNSUPPORTED   : u8 = 0x0D;
+    pub const RECORDSET     : u8 = 0x0E; // reserved, not supported
+    pub const XML_DOCUMENT  : u8 = 0x0F;
+    pub const TYPED_OBJECT  : u8 = 0x10;
     pub const AVMPLUS_OBJECT: u8 = 0x11;
 }
 
